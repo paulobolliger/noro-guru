26d5dfe841214967a00ea073df4a1d0da23c5185
paulobolliger <paulobolliger@gmail.com>
2026-04-09 15:46:25 -0300

    chore(types): add shared types entrypoint

---
+++ packages/types/src/index.ts
@@ -0,0 +1,9 @@
+export * from '../admin';
+export * from '../clientes';
+export * from '../control-plane';
+export * from '../control-plane-users';
+export * from '../financeiro';
+export * from '../noro-users';
+export * from '../supabase';
+
+export * from './blueprint';
